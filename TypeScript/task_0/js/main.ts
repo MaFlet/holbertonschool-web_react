5967132e39e83b4f6481dc07a84b4f372eb14267
MaFlet <mfletcher2808@hotmail.com>
2025-01-27 12:32:00 +1100

    Task 0

---
+++ TypeScript/task_0/js/main.ts
@@ -0,0 +1,10 @@
+interface Student {
+    firstName: string,
+    lastName: string,
+    age: number,
+    location: string,
+};
+
+let student1: string;
+let student2: string;
+let studentsList = [student1, student2];
