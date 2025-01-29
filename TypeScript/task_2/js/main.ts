9ef851b49c68b24a00a391dcf8c751175e7604d9
MaFlet <mfletcher2808@hotmail.com>
2025-01-29 16:25:43 +1100

    Task 6

--- TypeScript/task_2/js/main.ts
+++ TypeScript/task_2/js/main.ts
@@ -42,7 +42,18 @@ function createEmployee(salary: number | string): Director | Teacher {
     }
 }
 
+function isDirector(employee: Director | Teacher): employee is Director {
+    return (employee as Director).workDirectorTasks !== undefined;
+}
+
+function executeWork(employee: Director | Teacher): string {
+    if (isDirector(employee)) {
+        return employee.workDirectorTasks();
+    } else {
+        return employee.workTeacherTasks();
+    }
+}
+
 // Testing implementation
 console.log(createEmployee(200));
 console.log(createEmployee(1000));
-console.log(createEmployee('$500'));
